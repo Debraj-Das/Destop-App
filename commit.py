98ddab6421c745483fdf229219127359369e9664
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-05-29 21:04:54 +0530

    Web Scraping project for scraping the data from the website and storing it in the database

---
+++ commit.py
@@ -0,0 +1,19 @@
+import os
+import glob
+#* add the staged files to the commit and commit the changes with a message
+#* push the changes to the remote repository
+
+# / message for the commit
+commit_message = "Web Scraping project for scraping the data from the website and storing it in the database"
+
+
+# commit and push the changes to the remote repository
+
+# add the staged files to the commit
+os.system("git add .")
+# commit the changes with a message
+os.system(f"git commit -m \"{commit_message}\"")
+# push the changes to the remote repository
+os.system("git push origin master")
+
+print("All changes are commit and pushed to the remote repository\n\n")
